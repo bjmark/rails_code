e6ef9026ee300e0d2de555bc67de74af99a87c63
mark peng <bjmark@sina.com>
2012-01-31 19:20:43 +0800

    jquery_code.js

---
+++ jquery_code.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+		$('#selected-plays > li').addClass('horizontal');
+		});
+
+$(document).ready(function() {
+		$('#selected-plays > li').addClass('horizontal');
+		$('#selected-plays li:not(.horizontal)').addClass('sub-level');
+		});
+
+$('img[alt]')
+
+$(document).ready(function() {
+		$('a[href^="mailto:"]').addClass('mailto');
+		});
+
+$(document).ready(function() {
+		$('a[href^="mailto:"]').addClass('mailto');
+		$('a[href$=".pdf"]').addClass('pdflink');
+		});
+
+$(document).ready(function() {
+		$('a[href^="mailto:"]').addClass('mailto');
+		$('a[href$=".pdf"]').addClass('pdflink');
+		$('a[href^="http"][href*="henry"]')
+		.addClass('henrylink');
+		});
+
+$('div.horizontal:eq(1)')
